551972d4f636f8246aa35f365a4ee462aeeb19eb
michelle <michelle_lee100@hotmail.com>
2023-09-20 01:32:37 -0400

    added components folder, removed package lock, changed site name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React,  { useState } from 'react';
 import Globe from 'react-globe.gl';
 import './App.css';
 
 function App() {
+  const [atmosphere, setAtmosphere] = useState(0.3);
+
+  const increaseAtmosphere = (lat, long, event) => {
+    setAtmosphere(atmosphere + 0.05);
+  }
+
+  
   return (
     <Globe
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      atmosphereAltitude="0.2"
+
+      atmosphereAltitude={atmosphere.toString()}
+
+      onGlobeClick = {(increaseAtmosphere)}
     />
   );
 }
