6103673832936ebfb5c6bd34135d215b4bff185f
rnachaloor <rnachaloor@gmail.com>
2023-09-26 19:32:57 -0400

    added marker feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React,  { useState } from 'react';
+import Globe from 'react-globe.gl';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [points, setPoints] = useState([])
+  const mainGlobe = useRef();
+
+  function clickLabel(lat, lng) {
+    mainGlobe.current.pointOfView({ lat: lat, lng: lng, altitude: .5 })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Globe
+      ref={mainGlobe}
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      atmosphereAltitude="0.2"
+      labelsData={points}
+      labelLat={d => d.lat}
+      labelLng={d => d.lng}
+      labelText={d => d.name}
+      labelSize={1.5}
+      labelColor={() => 'yellow'}
+      labelIncludeDot={true}
+      labelDotRadius={1.25}
+      onLabelClick={(label, event, { lat, lng, altitude }) => clickLabel(lat, lng)}
+      onGlobeClick={({ lat, lng}, event) => setPoints([{
+        lng: lng,
+        lat: lat,
+        size: Math.random() / 3,
+        color: 'green',
+        name: "Atlanta"
+      }])}
+    />
   );
 }
 
