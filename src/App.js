7a641c8ca9250da2514410bd700f623de1b99c09
michelle <michelle_lee100@hotmail.com>
2023-09-28 00:53:07 -0400

    added marker feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,  { useState } from 'react';
+import React,  { useState, useRef } from 'react';
 import Globe from 'react-globe.gl';
 import './App.css';
 
@@ -6,10 +6,11 @@ import Button from './components/button.js';
 import Pop from './components/popup.js';
 
 function App() {
-  const [atmosphere, setAtmosphere] = useState(0.3);
+  const [points, setPoints] = useState([])
+  const mainGlobe = useRef();
 
-  const increaseAtmosphere = (lat, long, event) => {
-    setAtmosphere(atmosphere + 0.05);
+  function clickLabel(lat, lng) {
+    mainGlobe.current.pointOfView({ lat: lat, lng: lng, altitude: .5 })
   }
 
   const handleClick = (event) => {
@@ -19,17 +20,30 @@ function App() {
   return (
     <div>
     <Globe
+      ref={mainGlobe}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-
-      atmosphereAltitude={atmosphere.toString()}
-
-      onGlobeClick = {(increaseAtmosphere)}
-
-      backgroundColor ="#6D5BA1"
+      atmosphereAltitude="0.2"
+      labelsData={points}
+      labelLat={d => d.lat}
+      labelLng={d => d.lng}
+      labelText={d => d.name}
+      labelSize={1.5}
+      labelColor={() => 'yellow'}
+      labelIncludeDot={true}
+      labelDotRadius={1.25}
+      onLabelClick={(label, event, { lat, lng, altitude }) => clickLabel(lat, lng)}
+      onGlobeClick={({ lat, lng}, event) => setPoints([{
+        lng: lng,
+        lat: lat,
+        size: Math.random() / 3,
+        color: 'green',
+        name: "Atlanta"
+      }])}
     />
     
     <Pop trigger={<Button onClick={handleClick}>ADD LOCATION</Button>}>Add Location</Pop>
     </div>
+    
   );
 }
 
