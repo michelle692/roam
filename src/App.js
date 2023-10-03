33b447bbbad6d828c14c83fcb2322bf577922127
AlexT <alex.thummas66@gmail.com>
2023-10-03 18:54:09 -0400

    Minor UI changes

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ function App() {
     setButton1([false, false, !button1[2]]);
   }
 
+  const handleClick4 = () => {
+    setButton1([false, false, false]);
+  }
 
   return (
     <div>
@@ -42,9 +45,9 @@ function App() {
     <Button val={button1[0]} onClick={handleClick1} offset={'25vh'}>ADD LOCATION</Button>
     <Button val={button1[1]} onClick={handleClick2} offset={'calc(25vh + 50px)'}>TRAVEL WISHLIST</Button>
     <Button val={button1[2]} onClick={handleClick3} offset={'calc(25vh + 100px)'}>TRAVEL HISTORY</Button>
-    <Popup open={button1[0]}>ADD LOCATION</Popup>
-    <Popup open={button1[1]}>TRAVEL WISHLIST</Popup>
-    <Popup open={button1[2]}>TRAVEL HISTORY</Popup>
+    <Popup open={button1[0]} close={handleClick4}>ADD LOCATION</Popup>
+    <Popup open={button1[1]} close={handleClick4}>TRAVEL WISHLIST</Popup>
+    <Popup open={button1[2]} close={handleClick4}>TRAVEL HISTORY</Popup>
     
     <p className="Title">ROAM</p>
     </div>
--- src/components/popup.js
+++ src/components/popup.js
@@ -4,29 +4,41 @@ import Modal from 'react-modal';
 const popupStyle = {
     overlay: {
         margin: 'auto',
-        background: 'rgba(217, 217, 217, 0.90)',
-        paddingLeft: '200px',
-        paddingRight: '200px',
-        paddingTop: '100px',
-        paddingBottom: '100px',
-        borderRadius: '10px',
+        background: 'rgba(255, 255, 255, 0.5)',
+        border: '3px solid rgba(249, 249, 249, 0.78)',
         textAlign: 'center',
         position: 'absolute',
         top: '50%',
         left: '50%',
-        transform: 'translate(-50%, -50%)'
+        width: '40%',
+        height: '50%',
+        transform: 'translate(-60%, -50%)',
+        backdropFilter: 'blur(6px)',
     },
     content: {
         background: 'rgba(0, 0, 0, 0)',
         border: 'none',
-        fontFamily: 'Overpass Mono'
+        fontFamily: 'Overpass Mono',
+        fontSize: '20px',
+        fontWeight: 'bold'
     }
 }
+const exitButton = {
+    position: 'absolute',
+    right: '0%',
+    top: '0%',
+    borderRadius: '100%',
+    border: 'none',
+    width: '1.5rem',
+    height: '1.5rem',
+    background: 'rgba(255, 255, 255, 0.5)'
+}
 
-const Popup = ({children, open}) => {
+const Popup = ({children, open, close}) => {
     return (
         <Modal isOpen={open}  backdrop="static" style={popupStyle}>
             {children}
+            <button style={exitButton} onClick={close}>X</button>
         </Modal>
     )
 }
