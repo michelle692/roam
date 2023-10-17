7ae8d8b71d7e567bc24d1e8baf04bd4c559ab43d
chloe <chloeng73@gmail.com>
2023-10-17 19:26:35 -0400

    init routing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 import Button from './components/button.js';
 import Popup from './components/popup.js';
+import Information from './Information'
 
 import LocationPopup from './components/location_popup.js';
 import LargePopup from './components/largepopup.js';
 import Wishlist from './travel-wishlist.js';
 import History from './travel-history.js';
 import { createContext } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 export const citiesContext = createContext({
   citiesVisited:     [{
@@ -77,46 +79,48 @@ function App() {
   }
 
   return (
-    <div className='background'>
-    <Globe
-      ref={mainGlobe}
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-      backgroundColor ="rgba(0,0,0,0)"
-      atmosphereColor={'white'}
-      atmosphereAltitude="0.3"
-      labelsData={points}
-      labelLat={d => d.lat}
-      labelLng={d => d.lng}
-      labelText={d => d.name}
-      labelSize={1.5}
-      labelColor={() => 'yellow'}
-      labelIncludeDot={true}
-      labelDotRadius={1.25}
-      onLabelClick={(label, event, { lat, lng, altitude }) => clickLabel(lat, lng)}
-      onGlobeClick={({ lat, lng}, event) => setPoints([{
-        lng: lng,
-        lat: lat,
-        size: Math.random() / 3,
-        color: 'green',
-        name: "Atlanta"
-      }])}
-    />
-    
-    <Button val={button1[0]} onClick={handleClick1} offset={'25vh'}>ADD LOCATION</Button>
-    <Button val={button1[1]} onClick={handleClick2} offset={'calc(25vh + 50px)'}>TRAVEL WISHLIST</Button>
-    <Button val={button1[2]} onClick={handleClick3} offset={'calc(25vh + 100px)'}>TRAVEL HISTORY</Button>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={App} />
+        <Route path="/information" component={Information} />
+      </Routes>
 
-    
-    
-    <Wishlist openVal={button1[1]} closeVal={handleClick1} openVal2={button1[0]} closeVal2={handleClick4} />
-    <History openVal={button1[2]} closeVal={handleClick1} openVal2={button1[0]} closeVal2={handleClick4} />
-    <LocationPopup open={button1[0]} close={handleClick4} onChange={changeLocVal} submit={handleLocSubmit}>ADD LOCATION</LocationPopup>
+      <div className='background'>
+        <Globe
+          ref={mainGlobe}
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+          backgroundColor ="rgba(0,0,0,0)"
+          atmosphereColor={'white'}
+          atmosphereAltitude="0.3"
+          labelsData={points}
+          labelLat={d => d.lat}
+          labelLng={d => d.lng}
+          labelText={d => d.name}
+          labelSize={1.5}
+          labelColor={() => 'yellow'}
+          labelIncludeDot={true}
+          labelDotRadius={1.25}
+          onLabelClick={(label, event, { lat, lng, altitude }) => clickLabel(lat, lng)}
+          onGlobeClick={({ lat, lng}, event) => setPoints([{
+            lng: lng,
+            lat: lat,
+            size: Math.random() / 3,
+            color: 'green',
+            name: "Atlanta"
+          }])}
+        />
+        
+        <Button val={button1[0]} onClick={handleClick1} offset={'25vh'}>ADD LOCATION</Button>
+        <Button val={button1[1]} onClick={handleClick2} offset={'calc(25vh + 50px)'}>TRAVEL WISHLIST</Button>
+        <Button val={button1[2]} onClick={handleClick3} offset={'calc(25vh + 100px)'}>TRAVEL HISTORY</Button>
+        
+        <Wishlist openVal={button1[1]} closeVal={handleClick1} openVal2={button1[0]} closeVal2={handleClick4} />
+        <History openVal={button1[2]} closeVal={handleClick1} openVal2={button1[0]} closeVal2={handleClick4} />
+        <LocationPopup open={button1[0]} close={handleClick4} onChange={changeLocVal} submit={handleLocSubmit}>ADD LOCATION</LocationPopup>
 
-   
- 
-    <p className="Title">ROAM</p>
-    </div>
-    
+        <p className="Title">ROAM</p>
+      </div>
+    </Router>  
   );
 }
 
